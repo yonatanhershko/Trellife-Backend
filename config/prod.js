06af5057c863c94f5bd8b4f273dcabab7df79aca
Jonathan Dolan <yonidolan1@gmail.com>
2024-07-26 11:25:28 +0300

    add config to .gitignore

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://Dolevy:LcohFlBzMIVkYplO@cluster0.hn1rqkv.mongodb.net/',
+    dbURL: 'mongodb+srv://YoniDolan:08r8c38XnpfOm0CT@cluster0.hn1rqkv.mongodb.net/',
     dbName: 'trellifeDB',
 }
