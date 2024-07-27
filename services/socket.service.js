8ba0fd549fea01093e342f4edab970d85efe80d6
Jonathan Dolan <yonidolan1@gmail.com>
2024-07-27 19:38:56 +0300

    Add update watched board socket

--- services/socket.service.js
+++ services/socket.service.js
@@ -14,14 +14,14 @@ export function setupSocketAPI(http) {
         socket.on('disconnect', socket => {
             logger.info(`Socket disconnected [id: ${socket.id}]`)
         })
-        socket.on('chat-set-topic', topic => {
-            if (socket.myTopic === topic) return
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
-                logger.info(`Socket is leaving topic ${socket.myTopic} [id: ${socket.id}]`)
+        socket.on('set-watched-board', boardId => {
+            if (socket.watchedBoardId === boardId) return
+            if (socket.watchedBoardId) {
+                socket.leave(socket.watchedBoardId)
+                logger.info(`Socket is leaving board ${socket.watchedBoardId} [id: ${socket.id}]`)
             }
-            socket.join(topic)
-            socket.myTopic = topic
+            socket.join(boardId)
+            socket.watchedBoardId = boardId
         })
         socket.on('chat-send-msg', msg => {
             logger.info(`New chat msg from socket [id: ${socket.id}], emitting to topic ${socket.myTopic}`)
